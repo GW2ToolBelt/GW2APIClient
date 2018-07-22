8bcbc7fe6a6dc9928f0ba0f4cd11ac4487781aab
Leon Linhart <themrmilchmann@gmail.com>
2018-07-22 16:22:59 +0200

    chore: remove Gradle wrapper update script

--- .dev/update_gradle.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sh ./gradlew wrapper --gradle-distribution-url="$1" -q
-command git commit  -m "chore: update Gradle distro" -o ./gradle/wrapper/gradle-wrapper.jar ./gradle/wrapper/gradle-wrapper.properties ./gradlew ./gradlew.bat
