0d47d41b8b1e104ca0b7263ebdc1b4b370e4385f
Leon Linhart <themrmilchmann@gmail.com>
2019-01-25 18:56:48 +0100

    chore: update Kotlin {1.3.11 => 1.3.20}

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ import org.jetbrains.kotlin.gradle.plugin.*
 plugins {
     signing
     `maven-publish`
-    kotlin("multiplatform") version "1.3.11"
+    kotlin("multiplatform") version "1.3.20"
     id("kotlinx-serialization")
 }
 
