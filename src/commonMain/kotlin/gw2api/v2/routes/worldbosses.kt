85f9718f21097263c07c36b693daf6c90f698d8e
Leon Linhart <themrmilchmann@gmail.com>
2019-05-25 14:05:51 +0200

    feat(/v2/worldbosses): add endpoint
    Closes #151

---
+++ src/commonMain/kotlin/gw2api/v2/routes/worldbosses.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018-2019 Leon Linhart
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gw2api.v2.routes
+
+import gw2api.*
+import gw2api.extra.*
+import gw2api.jsonArrayParser
+import gw2api.query
+
+/**
+ * Queries the `/v2/worldbosses` endpoint.
+ *
+ * ## The /v2/worldbosses endpoint
+ *
+ * > This resource returns information about the world bosses that can be slain once per day.
+ *
+ * ```
+ * Paginated:           No
+ * Bulk expanded:       No
+ * Authenticated:       No
+ * Localized:           No
+ * Cache time:          24 hours
+ * ```
+ *
+ *
+ * Read more: [https://wiki.guildwars2.com/wiki/API:2/worldbosses]
+ *
+ * @return  the request that is used to query the API
+ *
+ * @since   0.1.0 (API: 2019-04-08)
+ */
+@GW2APIv2DataIds("worldbosses")
+fun gw2v2WorldBosses(): RequestBuilder<Collection<String>> = query(
+    endpoint = "/v2/worldbosses",
+    converter = jsonArrayParser(JSONStringParser)
+).setCacheTime(60 * 60 * 24, false)
