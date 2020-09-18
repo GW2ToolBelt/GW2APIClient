dd9d5e9369e7bf46b32ab7d664de18ae01d07c21
Leon Linhart <themrmilchmann@gmail.com>
2020-09-19 01:30:49 +0200

    build: update Kotlin {1.4.0 => 1.4.10}

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ import com.gw2tb.gw2apiclient.build.BuildType
 import com.gw2tb.gw2apiclient.build.codegen.*
 
 plugins {
-    kotlin("multiplatform") version "1.4.0" apply false
-    kotlin("plugin.serialization") version "1.4.0" apply false
+    kotlin("multiplatform") version "1.4.10" apply false
+    kotlin("plugin.serialization") version "1.4.10" apply false
     id("org.jetbrains.dokka") version "1.4.0-dev-62" apply false
 }
 
