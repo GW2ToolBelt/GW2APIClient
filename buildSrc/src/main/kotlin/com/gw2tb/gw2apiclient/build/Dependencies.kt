3dfcd1a21680934f921e3a0834e559536ba83794
Leon Linhart <themrmilchmann@gmail.com>
2021-02-25 15:07:58 +0100

    build: update kotlinx.serialization {1.0.1 => 1.1.0}

--- buildSrc/src/main/kotlin/com/gw2tb/gw2apiclient/build/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/gw2tb/gw2apiclient/build/Dependencies.kt
@@ -25,7 +25,7 @@ object Dependencies {
 
     // Shared depencencies
     val kotlinxCoroutinesVersion = "1.4.2-native-mt"
-    val kotlinxSerializationVersion = "1.0.1"
+    val kotlinxSerializationVersion = "1.1.0"
     val ktorVersion = "1.5.0"
 
     // JVM dependencies
