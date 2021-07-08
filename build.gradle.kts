2aa73f7a6ff18346b824be094d3ea3e1b2920d0d
Leon Linhart <themrmilchmann@gmail.com>
2021-07-08 19:52:11 +0200

    build: update dokka {1.4.32 => 1.5.0}

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ import com.gw2tb.gw2apiclient.build.tasks.*
 plugins {
     kotlin("multiplatform") version "1.5.20" apply false
     kotlin("plugin.serialization") version "1.5.20" apply false
-    id("org.jetbrains.dokka") version "1.4.32" apply false
+    id("org.jetbrains.dokka") version "1.5.0" apply false
 }
 
 allprojects {
